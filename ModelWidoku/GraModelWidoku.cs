f0f39ba21bbc140bfdb0809e8902fce972a5adce
kdpanetpl <kamil_dabkowski1@wp.pl>
2021-01-01 10:10:09 +0100

    initial commit

---
+++ ModelWidoku/GraModelWidoku.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkanoid.ModelWidoku
+{
+    class GraModelWidoku : INotifyPropertyChanged
+    {
+        #region properties
+
+        #endregion
+
+        #region ctor
+
+        #endregion
+
+
+        #region events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(params string[] nazwyWlasnosci)
+        {
+            if (PropertyChanged != null)
+            {
+                foreach (string nazwaWlasnosci in nazwyWlasnosci)
+                    PropertyChanged(this, new PropertyChangedEventArgs(nazwaWlasnosci));
+            }
+        }
+
+        #endregion
+    }
+}
